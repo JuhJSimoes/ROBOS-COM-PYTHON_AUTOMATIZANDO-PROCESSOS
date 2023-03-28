294591da097f7c9e96808e7624cf1f414ddfb782
JuhJSimoes <julianajsimoes@gmail.com>
2023-03-28 06:33:27 -0300

    Retornando o status do dominio pesquisado.

--- AUTOMACAO_PYTHON_SELENIUM/INTRODUCAO/bot_intro.py
+++ AUTOMACAO_PYTHON_SELENIUM/INTRODUCAO/bot_intro.py
@@ -3,20 +3,35 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+import pdb
+
 print("Iniciando bot \n")
 
-driver = webdriver.Chrome('AUTOMACAO_PYTHON_SELENIUM\\INTRODUCAO\\chromedriver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--incognito')
+#chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver = webdriver.Chrome('AUTOMACAO_PYTHON_SELENIUM\\INTRODUCAO\\chromedriver.exe', chrome_options=chrome_options)
 driver.get('https://registro.br/')
 driver.maximize_window()
-time.sleep(3)
 
+time.sleep(2)
+
+dominio = 'botcompythoneselenium.com.br'
 buscar_dominio = driver.find_element(By.ID, "is-avail-field") #Buscando o input PESQUISAR DOMINIO por elemento
 buscar_dominio.clear() #limpando o PESQUISAR DOMINIO
-buscar_dominio.send_keys('botsinpython.com.br') #digitando na barra de pesquisa
+buscar_dominio.send_keys(dominio) #digitando na barra de pesquisa
 buscar_dominio.send_keys(Keys.RETURN) #buscando (enter)
 
-resultados = buscar_dominio.find_elements(By.TAG_NAME, 'strong')
-print(resultados)
+time.sleep(3)
+
+resultados = driver.find_elements(By.TAG_NAME, "strong")
+
+for r in resultados:
+    if r.text == 'disponível':
+        print('\n O domínio: %s está %s' %(dominio, r.text.upper()))
+    elif r.text == 'não disponível':
+        print('\n O domínio: %s está %s' %(dominio, r.text.upper()))
 
 time.sleep(2)
 driver.close()
